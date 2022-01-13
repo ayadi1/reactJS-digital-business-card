60d72f52aad8f290049bdc802bd26eb76d38881c
ayadi1 <>
2022-01-13 21:41:46 +0100

    edit image path and add live preview link

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import Links from "./components/Links";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import img from "./assets/images/1627227732848.jpeg"
 function App() {
   return (
     <main className="app">
-      <div className="img"></div>
+      <div className="img" style={{ backgroundImage: `url(${img})` }}></div>
       <Links />
       <Main />
       <Footer />
